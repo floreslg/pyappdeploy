355a5d450af3765344a91e39f9fc04a377de2f9a
Gabriel Flores <gabriel.flores@tutanota.com>
2022-12-18 20:44:32 +0100

    set flask server as app

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ from flask import Flask
 import dash
 
 server = Flask(__name__)
-app = dash.Dash(
+dash_app = dash.Dash(
     __name__,
     server=server
 )
-app.layout = dash.html.Div("Hello")
+dash_app.layout = dash.html.Div("Hello")
 
+app = dash_app.server
 if __name__=='__main__':
-    app.run_server()
+    app.run()
